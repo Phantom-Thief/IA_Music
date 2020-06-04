8efbc3961a5760f283123c3b3cce289a992c6a4e
AndrosC <alexis.chevalier@edu.esiee.fr>
2020-06-04 13:20:27 +0200

    Dico_Accords_V2
    Ajout accords 4 notes+commentaires

--- musicologie/Dictionnaire_accord.py
+++ musicologie/Dictionnaire_accord.py
@@ -1,5 +1,7 @@
 from pyo import *
 
+
+#Accord de 3 notes
 def Accord3Maj(a) :
      b=Sine(freq=[midiToHz(a),midiToHz(a+4),midiToHz(a+7)] , mul=1).out(0)
      return b
@@ -15,3 +17,41 @@ def Accord3Dim(a) :
 def Accord3Aug(a) :
      b=Sine(freq=[midiToHz(a),midiToHz(a+3),midiToHz(a+6)] , mul=1).out(0)
      return b
+
+#Accord de 4 notes
+#Accord de septième de dominante
+def Accord4Dom(a):
+     b=Sine(freq=[midiToHz(a),midiToHz(a+4),midiToHz(a+7),midiToHz(a+10)] ,mul=1).out(0)
+     return b
+
+#Accord de septième mineure
+def Accord4Min(a):
+     b=Sine(freq=[midiToHz(a),midiToHz(a+3),midiToHz(a+7),midiToHz(a+10)] ,mul=1).out(0)
+     return b
+
+#Accord de septième mineure et quinte diminuée
+def Accord4MinDim(a):
+     b=Sine(freq=[midiToHz(a),midiToHz(a+3),midiToHz(a+6),midiToHz(a+10)] ,mul=1).out(0)
+     return b
+
+#Accord de septième majeure
+def Accord4Maj(a):
+     b=Sine(freq=[midiToHz(a),midiToHz(a+4),midiToHz(a+7),midiToHz(a+11)] ,mul=1).out(0)
+     return b
+
+#Accord de septième diminuée
+def Accord4Dim(a):
+     b=Sine(freq=[midiToHz(a),midiToHz(a+3),midiToHz(a+6),midiToHz(a+9)] ,mul=1).out(0)
+     return b
+
+#Accord de septième diminuée
+def Accord4MajMin(a):
+     b=Sine(freq=[midiToHz(a),midiToHz(a+3),midiToHz(a+7),midiToHz(a+11)] ,mul=1).out(0)
+     return b
+
+#Accord de septième majeure et quinte augmentée
+def Accord4MajAug(a):
+     b=Sine(freq=[midiToHz(a),midiToHz(a+4),midiToHz(a+8),midiToHz(a+11)] ,mul=1).out(0)
+     return b
+
+#Accord 
