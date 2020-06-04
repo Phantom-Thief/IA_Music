d0963da673ee64d80ec8e90354e4c85b1ab08ea1
Menchit-ai <thomas.menchi@edu.esiee.fr>
2020-06-04 16:09:46 +0200

    calcul de 3 distances différentes

--- Mouselogger.py
+++ Mouselogger.py
@@ -1,4 +1,6 @@
 import pynput
+from scipy.spatial import distance
+import numpy as np
 from pynput.mouse import Button,Listener
 from datetime import datetime
 
@@ -7,7 +9,7 @@ class Mouselog:
         """
         move : list with all movement from the mouse
         clic : list with all clic and their position
-        scroll : list with scroll's position and delta created by scrolling
+        scroll : list with scroll's position and the direction of the scrolling
         listener : object used to listen mouse's activities
         """
         self.move = []
@@ -30,25 +32,56 @@ class Mouselog:
         """
         return ( len(self.move),len(self.clic),len(self.scroll) )
 
+    def getTravelDistance(self):
+        """
+        return distance between the first and the last cursor's position
+        """
+        p1 = self.move[0][1]
+        p2 = self.move[-1][1]
+        return distance.euclidean(p1,p2)
+
+    def getCumulTravelDistance(self):
+        """
+        return the whole distance traveled by the cursor
+        """
+        dist = []
+        for i in range(len(self.move)-1):
+            p1 = self.move[i][1]
+            p2 = self.move[i+1][1]
+            dist.append( distance.euclidean(p1,p2) )
+        return np.sum(dist)
+
+    def getMaxDistance(self):
+        """
+        return the distance between the two farthest points
+        """
+        points = []
+        for i in self.move:
+            points.append(i[1])
+        p1 = np.min(points)
+        p2 = np.max(points)
+        return distance.euclidean(p1,p2)
+
+
     def on_move(self,x,y):
         """
         add logs and movement of mouse when it moves to the move list
         """
-        log = str(datetime.now().time()) + " " + str((x,y))
+        log = ( datetime.now().time(), (x,y) )
         self.move.append(log)
 
     def on_click(self,x,y,button,pressed):
         """
         add logs and mouse's coordonates when a mouse's button is pressed or release
         """
-        log = str(datetime.now().time()) + " " + str((x,y))
+        log = ( datetime.now().time(), (x,y) )
         self.clic.append(log)
 
     def on_scroll(self,x,y,dx,dy):
         """
-        add logs, mouse's coordonates and movement caused by scrolling when the mousewheel is used
+        add logs, mouse's coordonates and movement's direction caused by scrolling when the mousewheel is used
         """
-        log = str(datetime.now().time()) + " " + str((x,y,dx,dy))
+        log = ( datetime.now().time(), (x,y,dx,dy) )
         self.scroll.append(log)
 
     def write_on_file(self, filename):
@@ -60,15 +93,15 @@ class Mouselog:
         """
         with open(filename,'w') as f:
             for mov in self.move:
-                f.write(mov)
+                f.write(str(mov))
                 f.write("\n")
             f.write(";\n")
             for cli in self.clic:
-                f.write(cli)
+                f.write(str(cli))
                 f.write("\n")
             f.write(";\n")
             for scro in self.scroll:
-                f.write(scro)
+                f.write(str(scro))
                 f.write("\n")
             f.write(";")
 
