822bf19a0ed95c62dfabdbb588287b0a560198a0
Menchit-ai <thomas.menchi@edu.esiee.fr>
2020-06-04 17:10:35 +0200

    maj commentaires

--- getImage.py
+++ getImage.py
@@ -12,14 +12,18 @@ import datetime
 
 class GetImage(Thread):
     
-    #constructeur de la classe GetImage avec comme attribut l'image qu'on veut stocker
     def __init__(self):
+        """
+        constructeur de la classe GetImage avec comme attribut l'image qu'on veut stocker
+        """
         Thread.__init__(self)
         self.image = None
     
-    #méthode qui s'éxécute à chaque fois que l'on lance le Thread, ici le Thread prend un screen pendant 10 min 
-    #toutes les 0.5s
     def run(self):
+        """
+        méthode qui s'éxécute à chaque fois que l'on lance le Thread, ici le Thread prend un screen pendant 10 min 
+        toutes les 0.5s
+        """
         for n in range(1200):
             time.sleep(0.5)
             self.takeScreen("image.png")
