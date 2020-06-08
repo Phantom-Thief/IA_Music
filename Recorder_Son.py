1ffdbc3668876fb4ef28bf8644435158413a4c4f
GuerroisTheo <theo.guerrois@edu.esiee.fr>
2020-06-08 14:55:32 +0200

    Rajout de la fréquence max
    mise a jour des attributs + amplitude max

--- Recorder_Son.py
+++ Recorder_Son.py
@@ -3,6 +3,7 @@ import pyaudio
 #import pour la creation de .wav
 import wave
 import time
+import _portaudio as pa
 #Décommenter cette ligne pour plt les graphiques
 import matplotlib.pyplot as plt
 import numpy
@@ -26,15 +27,15 @@ class RecordingFile(object):
     """
     def __init__(self, fname, mode, channels, 
                 rate, frames_per_buffer):
-        self.fname = fname
-        self.mode = mode
-        self.channels = channels
-        self.rate = rate
-        self.frames_per_buffer = frames_per_buffer
+        self.a_fname = fname
+        self.a_mode = mode
+        self.a_channels = channels
+        self.a_rate = rate
+        self.a_frames_per_buffer = frames_per_buffer
         self._pa = pyaudio.PyAudio()
-        self.wavefile = self._prepare_file(self.fname, self.mode)
+        self.a_wavefile = self._prepare_file(self.a_fname, self.a_mode)
         self._stream = None
-        self.abs_data = None
+        self.a_abs_data = None
 
     def __enter__(self):
         return self
@@ -45,10 +46,10 @@ class RecordingFile(object):
     def start_recording(self):
         # Use a stream with a callback in non-blocking mode
         self._stream = self._pa.open(format=pyaudio.paInt16,
-                                        channels=self.channels,
-                                        rate=self.rate,
+                                        channels=self.a_channels,
+                                        rate=self.a_rate,
                                         input=True,
-                                        frames_per_buffer=self.frames_per_buffer,
+                                        frames_per_buffer=self.a_frames_per_buffer,
                                         stream_callback=self.get_callback())
         self._stream.start_stream()
         return self
@@ -59,35 +60,39 @@ class RecordingFile(object):
 
     def get_callback(self):
         def callback(in_data, frame_count, time_info, status):
-            self.wavefile.writeframes(in_data)
+            self.a_wavefile.writeframes(in_data)
             return in_data, pyaudio.paContinue
         return callback
 
     def amplitude(self,plot):
-        data, samplerate = sf.read(self.fname)
-        self.abs_data = abs(data)
+        data, samplerate = sf.read(self.a_fname)
+        self.a_abs_data = abs(data)
 
         if(plot):
             #décommenter la section ci-dessous pour voir les graphiquesS
-            plt.plot(self.abs_data)
+            plt.plot(self.a_abs_data)
             plt.ylabel('Amplitude')
             plt.show()
             
-        return self.abs_data
+        return self.a_abs_data
 
     def moyenne(self):
-        return numpy.mean(self.abs_data)
+        return numpy.mean(self.a_abs_data)
+
+    def extremum(self):
+        return self.a_abs_data.max()
+    
 
     def close(self):
         self._stream.close()
         self._pa.terminate()
-        self.wavefile.close()
+        self.a_wavefile.close()
 
     def _prepare_file(self, fname, mode='wb'):
         wavefile = wave.open(fname, mode)
-        wavefile.setnchannels(self.channels)
+        wavefile.setnchannels(self.a_channels)
         wavefile.setsampwidth(self._pa.get_sample_size(pyaudio.paInt16))
-        wavefile.setframerate(self.rate)
+        wavefile.setframerate(self.a_rate)
         return wavefile
 
 
@@ -98,6 +103,6 @@ print("Lancement du son")
 time.sleep(5)
 print("hop hop on stop le record")
 S.stop_recording()
-print(S.amplitude(plot = True))
+print(S.amplitude(plot = False))
 
-print(S.moyenne())
+print(S.extremum())
