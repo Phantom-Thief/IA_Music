3549a2cf406fe1915a4cc20b4e96c5f5d25b6260
GuerroisTheo <theo.guerrois@edu.esiee.fr>
2020-06-09 16:55:51 +0200

    Mise commentaires
    Commentaire + respect des regles

--- Recorder_Son.py
+++ Recorder_Son.py
@@ -1,40 +1,41 @@
 """
-Tous les imports pour pouvoir lancer le programme
+All imports to be able to launch the program
+import of the module that allows the sound record 'pyaudio'
+import for the creation of .wav 'wave'
+importation pour plot (format de test) 'matplotlib.pyplot as plt'
+import for sound processing 'soundfile as sf'
 """
-#importation du module qui permet le record du son
 import pyaudio
-#import pour la creation de .wav
 import wave
 import time
 import _portaudio as pa
-# cette ligne pour plt les graphiques
 import matplotlib.pyplot as plt
 import numpy
 import soundfile as sf
 
 class Recorder(object):
-    """Création de la classe Recorder.
+    """Creating the Recorder class. This class opens a .wav file that will store the recorded sound file.
     """
-    def __init__(self, channels=1, rate=44100, frames_per_buffer=1024):
-        self.a_channels = channels
-        self.a_rate = rate
-        self.a_frames_per_buffer = frames_per_buffer
+    def __init__(self, p_channels=1, p_rate=44100, p_frames_per_buffer=1024):
+        self.a_channels=p_channels
+        self.a_rate=p_rate
+        self.a_frames_per_buffer=p_frames_per_buffer
 
-    def open(self, fname, mode='wb'):
-        return RecordingFile(fname, mode, self.a_channels, self.a_rate,
+    def open(self, p_fname, mode='wb'):
+        return RecordingFile(p_fname, mode, self.a_channels, self.a_rate,
                             self.a_frames_per_buffer)
 
 class RecordingFile(object):
-    """ Création de la classe RecordingFile. Cette classe permet l'enregistrement du microphone pendant  secondes.
-    L'enregistrement peut etre lancer avec la fonction start_recording() puis enregistré avec la fonction write_on_file()
+    """ Creation of the RecordingFile class. This class allows the recording of the microphone during seconds.
+    The recording can be started with the function start_recording() and then recorded with the function write_on_file().
     """
-    def __init__(self, fname, mode, channels, 
-                rate, frames_per_buffer):
-        self.a_fname = fname
-        self.a_mode = mode
-        self.a_channels = channels
-        self.a_rate = rate
-        self.a_frames_per_buffer = frames_per_buffer
+    def __init__(self, p_fname, p_mode, p_channels, 
+                p_rate, p_frames_per_buffer):
+        self.a_fname = p_fname
+        self.a_mode = p_mode
+        self.a_channels = p_channels
+        self.a_rate = p_rate
+        self.a_frames_per_buffer = p_frames_per_buffer
         self._pa = pyaudio.PyAudio()
         self.a_wavefile = self._prepare_file(self.a_fname, self.a_mode)
         self._stream = None
@@ -45,9 +46,13 @@ class RecordingFile(object):
 
     def __exit__(self, exception, value, traceback):
         self.close()
-
+    
+    """
+    start_recording' is used to start recording.
+    Use a stream with a callback in non-blocking mode
+    """
     def start_recording(self):
-        # Use a stream with a callback in non-blocking mode
+        
         self._stream = self._pa.open(format=pyaudio.paInt16,
                                         channels=self.a_channels,
                                         rate=self.a_rate,
@@ -57,31 +62,46 @@ class RecordingFile(object):
         self._stream.start_stream()
         return self
 
+    """
+    arrête l'enregistrement
+    """
     def stop_recording(self):
         self._stream.stop_stream()
         return self
 
+
+    """
+    callback' is a function that allows you to launch our program in non-blocking mode.
+    """
     def get_callback(self):
         def callback(in_data, frame_count, time_info, status):
             self.a_wavefile.writeframes(in_data)
             return in_data, pyaudio.paContinue
         return callback
 
-    def amplitude(self,plot=False):
+    """
+    function that calculates the amplitudes of the sound placed in input, calculates the amplitudes
+    """
+    def amplitude(self,p_plot=False):
         data, samplerate = sf.read(self.a_fname)
         self.a_abs_data = abs(data)
 
-        if(plot):
-            #décommenter la section ci-dessous pour voir les graphiques
+        if(p_plot):
             plt.plot(self.a_abs_data)
             plt.ylabel('Amplitude')
             plt.show()
             
         return self.a_abs_data
 
+    """
+    return the average of the calculated amplitudes
+    """
     def moyenne(self):
         return numpy.mean(self.a_abs_data)
 
+    """
+    return
+    """
     def extremum(self):
         return self.a_abs_data.max()
     
@@ -91,8 +111,8 @@ class RecordingFile(object):
         self._pa.terminate()
         self.a_wavefile.close()
 
-    def _prepare_file(self, fname, mode='wb'):
-        wavefile = wave.open(fname, mode)
+    def _prepare_file(self, p_fname, mode='wb'):
+        wavefile = wave.open(p_fname, mode)
         wavefile.setnchannels(self.a_channels)
         wavefile.setsampwidth(self._pa.get_sample_size(pyaudio.paInt16))
         wavefile.setframerate(self.a_rate)
