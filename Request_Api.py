a17b5cbfeb68babe9c65a5af25a4f274efd2289e
GuerroisTheo <theo.guerrois@edu.esiee.fr>
2020-06-09 15:57:48 +0200

    creation def sortie et remisezero
    création de 2 fonctions

--- Request_Api.py
+++ Request_Api.py
@@ -9,8 +9,10 @@ class Requests_Api():
     def __init__(self, p_request = "https://127.0.0.1:2999/liveclientdata/allgamedata", p_AllData = None):
         self.a_request = p_request
         self.a_AllData = requests.get(self.a_request, verify = False).json()
+        self.a_query = None
+        self.List_AllData = []
 
-    def select(self,obj='activePlayer'):
+    def select(self):
         """
         select an object in ['activePlayer', 'events', 'gameData', 'allPlayers']
         activePlayer is a dictionnary with keys : ['abilities', 'championStats', 'currentGold', 'fullRunes', 'level', 'summonerName']
@@ -18,8 +20,25 @@ class Requests_Api():
         gameData is a dictionnary with keys ['gameMode', 'gameTime', 'mapName', 'mapNumber', 'mapTerrain']
         allPlayers is a list with each player and their info (notable : isDead(bool),dict of player's items, team)
         """
-        query = self.a_AllData[obj] 
-        return query
+        a_query = self.a_AllData
+        return a_query
+
+    def sortie(self):
+        """Select dans scores les 'kills' et 'assists' de la personne current
+        """
+        self.List_AllData.append(a_query['scores']['kills'])
+        self.List_AllData.append(a_query['scores']['assists'])
+
+        """Select dans championStats la 'currentHealth'
+        """
+        self.List_AllData.append(a_query['championStats']['currentHealth'])
+
+        """Select dans event le dernier event
+        """
+        self.List_AllData.append(a_query['events']['Events'])
+
+    def remiseZero(self):
+        self.List_AllData[:] = []
 
     def update(self):
         self.a_AllData = requests.get(self.a_request, verify = False).json()
