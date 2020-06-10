f172605069c32ebf98a4d5884ac4454f8a135e83
JKacimi <57537290+Phantom-Thief@users.noreply.github.com>
2020-06-10 16:34:34 +0200

    Convention

--- repeatedTime.py
+++ repeatedTime.py
@@ -3,27 +3,41 @@ import time
 
 class RepeatedTimer(object):
   def __init__(self, interval, function, *args, **kwargs):
-    self._timer = None
-    self.interval = interval
-    self.function = function
-    self.args = args
-    self.kwargs = kwargs
-    self.is_running = False
-    self.next_call = time.time()
+    """
+    The builder of the 'repeatedTime' class.
+    The purpose of this class is to execute a "function" every x seconds put in the "interval" parameter.
+    The execution of the function is non-blocking and allow other threads to execute at the same time.
+    The constructor call the "run" function by itself so no need to call it when you create your 
+    repeatedTime object
+    """
+    self.a_time = None
+    self.a_interval = interval
+    self.a_function = function
+    self.a_args = args
+    self.a_kwargs = kwargs
+    self.a_is_running = False
+    self.a_next_call = time.time()
     self.start()
 
   def _run(self):
-    self.is_running = False
+    
+    self.a_is_running = False
     self.start()
-    self.function(*self.args, **self.kwargs)
+    self.a_function(*self.a_args, **self.a_kwargs)
 
   def start(self):
-    if not self.is_running:
-      self.next_call += self.interval
-      self._timer = threading.Timer(self.next_call - time.time(), self._run)
-      self._timer.start()
-      self.is_running = True
+    """
+    no need to call this function with your object
+    """
+    if not self.a_is_running:
+      self.a_next_call += self.a_interval
+      self.a_time = threading.Timer(self.a_next_call - time.time(), self._run)
+      self.a_time.start()
+      self.a_is_running = True
 
   def stop(self):
-    self._timer.cancel()
-    self.is_running = False
+     """
+    use this function to stop your thread whenever 
+    """
+    self.a_time.cancel()
+    self.a_is_running = False
