86565332c3d80317625fc185548764bcb09a7f02
GuerroisTheo <theo.guerrois@edu.esiee.fr>
2020-06-10 16:41:47 +0200

    Convention PEP257
    Convention PEP 257

--- repeatedTime.py
+++ repeatedTime.py
@@ -3,12 +3,12 @@ import time
 
 class RepeatedTimer(object):
   def __init__(self, interval, function, *args, **kwargs):
-    """
-    The builder of the 'repeatedTime' class.
+    """The builder of the 'repeatedTime' class.
+    
     The purpose of this class is to execute a "function" every x seconds put in the "interval" parameter.
     The execution of the function is non-blocking and allow other threads to execute at the same time.
     The constructor call the "run" function by itself so no need to call it when you create your 
-    repeatedTime object
+    repeatedTime object.
     
     """
     self.a_time = None
@@ -21,15 +21,12 @@ class RepeatedTimer(object):
     self.start()
 
   def _run(self):
-    
     self.a_is_running = False
     self.start()
     self.a_function(*self.a_args, **self.a_kwargs)
 
   def start(self):
-    """
-    no need to call this function with your object
-    """
+    """No need to call this function with your object."""
     if not self.a_is_running:
       self.a_next_call += self.a_interval
       self.a_time = threading.Timer(self.a_next_call - time.time(), self._run)
@@ -37,8 +34,6 @@ class RepeatedTimer(object):
       self.a_is_running = True
 
   def stop(self):
-     """
-    use this function to stop your thread whenever 
-    """
+    """Use this function to stop your thread whenever."""
     self.a_time.cancel()
     self.a_is_running = False
