40abe0c9e60c99f2dc3eb3fd682515a3d08f65cd
GuerroisTheo <theo.guerrois@edu.esiee.fr>
2020-06-10 10:22:16 +0200

    Mise en convention
    Convention (commentaire)

--- Request_Api.py
+++ Request_Api.py
@@ -20,25 +20,31 @@ class Requests_Api():
         gameData is a dictionnary with keys ['gameMode', 'gameTime', 'mapName', 'mapNumber', 'mapTerrain']
         allPlayers is a list with each player and their info (notable : isDead(bool),dict of player's items, team)
         """
-        a_query = self.a_AllData
-        return a_query
+        self.a_query = self.a_AllData
+        return self.a_query
 
-    def sortie(self):
-        """Select dans scores les 'kills' et 'assists' de la personne current
+    def output(self):
+        """Select in scores the 'kills' and 'assists' of the current person.
         """
         self.List_AllData.append(self.a_AllData['scores']['kills'])
         self.List_AllData.append(self.a_AllData['scores']['assists'])
 
-        """Select dans championStats la 'currentHealth'
+        """Select in championStats the 'currentHealth'
         """
         self.List_AllData.append(self.a_AllData['championStats']['currentHealth'])
 
-        """Select dans event le dernier event
+        """Select in event the last event
         """
         self.List_AllData.append(self.a_AllData['events']['Events'][-1])
 
+    """
+    Empty the list 'List_AllData'.
+    """
     def reset(self):
         self.List_AllData[:] = []
 
+    """
+    restart the 'allData' query
+    """
     def update(self):
         self.a_AllData = requests.get(self.a_request, verify = False).json()
