ece84fdfee8b9d4c0ecd4ebce389a88fe94b92dd
GuerroisTheo <theo.guerrois@edu.esiee.fr>
2020-06-10 11:47:34 +0200

    Convention PEP257 + supp de select
    Rajour de commentaire/suppresion de select()

--- Request_Api.py
+++ Request_Api.py
@@ -7,44 +7,37 @@ urllib3.disable_warnings()
 class Requests_Api():
 
     def __init__(self, p_request = "https://127.0.0.1:2999/liveclientdata/allgamedata", p_AllData = None):
+        """The builder of the 'Requests_Api' class.
+
+        This class makes an 'allgamedata' request to the server of a Lol game (the game must be launched). 
+        The response returns all the data on the current game stored in 'a_query'/
+        The chosen objects will be stored in 'a_list_AllData'.
+        To make this request it is necessary to have a key API linked to your Lol account.
+        
+        """
         self.a_request = p_request
         self.a_AllData = requests.get(self.a_request, verify = False).json()
-        self.a_query = None
-        self.List_AllData = []
-
-    def select(self):
-        """
-        select an object in ['activePlayer', 'events', 'gameData', 'allPlayers']
-        activePlayer is a dictionnary with keys : ['abilities', 'championStats', 'currentGold', 'fullRunes', 'level', 'summonerName']
-        events is a list of events with their id, name, EventTime
-        gameData is a dictionnary with keys ['gameMode', 'gameTime', 'mapName', 'mapNumber', 'mapTerrain']
-        allPlayers is a list with each player and their info (notable : isDead(bool),dict of player's items, team)
-        """
-        self.a_query = self.a_AllData
-        return self.a_query
+        self.a_list_AllData = []
 
     def output(self):
-        """Select in scores the 'kills' and 'assists' of the current person.
-        """
-        self.List_AllData.append(self.a_AllData['scores']['kills'])
-        self.List_AllData.append(self.a_AllData['scores']['assists'])
+        """Select an object in ['activePlayer', 'events', 'gameData', 'allPlayers'].
+        
+        'activePlayer' is a dictionnary with keys : ['abilities', 'championStats', 'currentGold', 'fullRunes', 'level', 'summonerName'].
+        'events' is a list of events with their id, name, EventTime.
+        'gameData' is a dictionnary with keys ['gameMode', 'gameTime', 'mapName', 'mapNumber', 'mapTerrain']
+        'allPlayers' is a list with each player and their info (notable : isDead(bool),dict of player's items, team).
+        We are storing the 'kills', 'assists', 'currentHealth of the current person and the last event in 'a_list_AllData'.
 
-        """Select in championStats the 'currentHealth'
         """
-        self.List_AllData.append(self.a_AllData['championStats']['currentHealth'])
+        self.a_list_AllData.append(self.a_AllData['scores']['kills'])
+        self.a_list_AllData.append(self.a_AllData['scores']['assists'])
+        self.a_list_AllData.append(self.a_AllData['championStats']['currentHealth'])
+        self.a_list_AllData.append(self.a_AllData['events']['Events'][-1])
 
-        """Select in event the last event
-        """
-        self.List_AllData.append(self.a_AllData['events']['Events'][-1])
-
-    """
-    Empty the list 'List_AllData'.
-    """
     def reset(self):
-        self.List_AllData[:] = []
+        """Empty the list 'a_list_AllData'."""
+        self.a_list_AllData[:] = []
 
-    """
-    restart the 'allData' query
-    """
     def update(self):
+        """Restart the 'allData' query."""
         self.a_AllData = requests.get(self.a_request, verify = False).json()
