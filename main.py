099d9ec2b932f41157dea28078253c144789a391
Menchit-ai <thomas.menchi@edu.esiee.fr>
2020-06-15 11:40:08 +0200

    début ia musique

--- main.py
+++ main.py
@@ -9,14 +9,16 @@ import Request_Api as api
 import scipy
 import repeatedTime
 import musicologie
+import pymix
 
+g_py = None
 g_klog = None
 g_mlog = None
 g_getApi = None
 g_getS = None
 g_allData = []
 g_rt = None
-g_ApiActive=True
+g_ApiActive=False
 g_file = 'rawdata.csv'
 
 def main():
@@ -27,21 +29,23 @@ def main():
 
 def init():
     """This function instantiates all our global variables."""
-    global g_klog, g_mlog, g_getApi, g_getS, g_rt, g_ApiActive
+    global g_klog, g_mlog, g_getApi, g_getS, g_rt, g_ApiActive, g_py
     g_klog = keylog.KeyLogger()
     g_mlog = Mouselogger.Mouselog()
     g_rt = repeatedTime.RepeatedTimer(1,dataHooker)
+    g_py = pymix.Pymix()
     if g_ApiActive : g_getApi = api.Requests_Api()
     # g_getS = Recorder().open('Sortie_GetS.wav')
     print("Initialize")
 
 def startAll():
     """Starts listening to the keyboard+mouse and recording the voice."""
-    global g_klog, g_mlog, g_getS, g_ApiActive
+    global g_klog, g_mlog, g_getS, g_ApiActive, g_py
     g_klog.start()
     g_mlog.start()
     # g_getS.start_recording()
     if g_ApiActive : g_getApi.update()
+    g_py.add_feeling('calm')
     print("All Started")
 
 def dataHooker():
@@ -73,7 +77,7 @@ def dataHooker():
         print()
         resetAll()   
         # g_getS.start_recording()
-    
+        print(ia())
     else:
         stopAll()
 
@@ -102,6 +106,17 @@ def resetAll():
     g_klog.reset()
     g_mlog.reset()
     if g_ApiActive : g_getApi.reset()
+
+def ia():
+    global g_py
+    tab = g_allData[-2:]
+    label = [ tab[0][-1],tab[1][-1] ]
+    if label[0] == label[1]:
+        if not (label[1] == g_py.state()):
+            g_py.kill_feeling(g_py.state(),fade_out=3000)
+            g_py.add_feeling(int(label[1]),fade_in=3000)
+
+    return label
 
 if __name__ == "__main__":
     main()
