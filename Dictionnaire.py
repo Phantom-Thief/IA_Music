82e2dc74ee161bdcd9b3bdb5f47da6eb3c3043b8
GuerroisTheo <theo.guerrois@edu.esiee.fr>
2020-06-16 15:20:49 +0200

    rajout des autres events
    rajout des events en commentaire

--- Dictionnaire.py
+++ Dictionnaire.py
@@ -1,10 +1,18 @@
 pyApi={"DragonKillORDER" : 0, "BaronKillORDER" : 1, "AceORDER" : 2}
+#pyApi["TurretKilledORDER"]
+#pyApi["InhibKilledORDER"]
+#pyApi["HeraldKillORDER"]
+#pyApi["ChampionKillORDER"]
+#pyApi["MultikillORDER"]
+#pyApi["ORDER"]
+
 pyApi["DragonKillCHAOS"] = 3
 pyApi["BaronKillCHAOS"] = 4
 pyApi["AceCHAOS"] = 5
-#pyApi[]
-#pyApi[]
-#pyApi[]
-#pyApi[]
-#pyApi[]
-#pyApi[]
+#pyApi["TurretKilledCHAOS"]
+#pyApi["InhibKilledCHAOS"]
+#pyApi["HeraldKillCHAOS"]
+#pyApi["ChampionKillCHAOS"]
+#pyApi["MultikillCHAOS"]
+#pyApi["CHAOS"]
+
