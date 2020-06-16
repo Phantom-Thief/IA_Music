5e146d3a018ec55aab14c75854c2b1d49b138a9c
GuerroisTheo <theo.guerrois@edu.esiee.fr>
2020-06-16 15:25:55 +0200

    changement dictionnaire

--- Dictionnaire.py
+++ Dictionnaire.py
@@ -1,18 +1,18 @@
 pyApi={"DragonKillORDER" : 0, "BaronKillORDER" : 1, "AceORDER" : 2}
-#pyApi["TurretKilledORDER"]
-#pyApi["InhibKilledORDER"]
-#pyApi["HeraldKillORDER"]
-#pyApi["ChampionKillORDER"]
-#pyApi["MultikillORDER"]
+pyApi["TurretKilledORDER"]=3
+pyApi["InhibKilledORDER"]=3
+pyApi["HeraldKillORDER"]=3
+pyApi["ChampionKillORDER"]=3
+pyApi["MultikillORDER"]=3
 #pyApi["ORDER"]
 
-pyApi["DragonKillCHAOS"] = 3
-pyApi["BaronKillCHAOS"] = 4
-pyApi["AceCHAOS"] = 5
-#pyApi["TurretKilledCHAOS"]
-#pyApi["InhibKilledCHAOS"]
-#pyApi["HeraldKillCHAOS"]
-#pyApi["ChampionKillCHAOS"]
-#pyApi["MultikillCHAOS"]
+pyApi["DragonKillCHAOS"]=4
+pyApi["BaronKillCHAOS"]=5
+pyApi["AceCHAOS"]=6
+pyApi["TurretKilledCHAOS"]=7
+pyApi["InhibKilledCHAOS"]=7
+pyApi["HeraldKillCHAOS"]=7
+pyApi["ChampionKillCHAOS"]=7
+pyApi["MultikillCHAOS"]=7
 #pyApi["CHAOS"]
 
