67da367a4d9ed78685a137dd1134a0e2befeb8b3
AndrosC <alexis.chevalier@edu.esiee.fr>
2020-06-16 12:55:29 +0200

    Ajustement IA_CLassification
    Correction ; et  limit bound (x,y)

--- test/IA_classification.py
+++ test/IA_classification.py
@@ -1,20 +1,20 @@
 #IA de classification 
 from numpy import loadtxt
-from keras.models import sequential
+from keras.models import Sequential
 from keras.layers import Dense
 
 
 # load the dataset
-dataset = loadtxt('rawdata_A_6.csv', delimiter=',')
+dataset = loadtxt('rawdata_A_6.csv', delimiter=';')
 
 # split into input (X) and output (y) variables
-X = dataset[:,0:8]
-y = dataset[:,8]
+X = dataset[:,0:6]
+y = dataset[:,-1]
 
 # define the keras model
-model = sequential()
-model.add(Dense(12, input_dim=8, activation='relu'))
-model.add(Dense(8, activation='relu'))
+model = Sequential()
+model.add(Dense(12, input_dim=6, activation='relu'))
+model.add(Dense(6, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 
 # compile the keras model
@@ -27,3 +27,14 @@ model.fit(X, y, epochs=150, batch_size=10)
 _, accuracy = model.evaluate(X, y)
 print('Accuracy: %.2f' % (accuracy*100))
 
+# make probability predictions with the model
+predictions = model.predict(X)
+# round predictions 
+rounded = [round(x[0]) for x in predictions]
+
+# make class predictions with the model
+predictions = model.predict_classes(X)
+
+# summarize the first 5 cases
+for i in range(5):
+	print('%s => %d (expected %d)' % (X[i].tolist(), predictions[i], y[i]))
