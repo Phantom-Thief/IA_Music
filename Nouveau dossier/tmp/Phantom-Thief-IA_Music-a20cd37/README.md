f21a66332ccd2a1b03c91252b139418581041d3f
Menchit-ai <thomas.menchi@edu.esiee.fr>
2020-06-26 11:04:22 +0200

    structure

--- Nouveau dossier/tmp/Phantom-Thief-IA_Music-a20cd37/README.md
+++
@@ -1,2 +0,0 @@
-# IA_Music
-Zone de Test pour réaliser en un seul code toutes les acquisitions nécessaire pour notre futur IA
--- Nouveau dossier/tmp/Phantom-Thief-IA_Music-a20cd37/concatenante.py
+++
@@ -1,9 +0,0 @@
-import glob
-import os
-
-def concatenate(path):
-    for i in glob.glob(path +"/*.csv"):
-        cmd = 'copy final.csv+'+i+' final.csv'
-        os.system(cmd)
-
-concatenate(".")
