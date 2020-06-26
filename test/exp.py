2bdc7895fc4465049723c433e94091db17099796
Menchit-ai <thomas.menchi@edu.esiee.fr>
2020-06-26 14:08:48 +0200

    updater

--- test/exp.py
+++ test/exp.py
@@ -1,6 +1,50 @@
+# https://sookocheff.com/post/tools/downloading-directories-of-code-from-github-using-the-github-api/
+
 from github import Github
-import github
+import base64
 
-g = Github('','')
+def get_sha_for_tag(repository, tag):
+    """
+    Returns a commit PyGithub object for the specified repository and tag.
+    """
+    branches = repository.get_branches()
+    matched_branches = [match for match in branches if match.name == tag]
+    if matched_branches:
+        return matched_branches[0].commit.sha
 
-repo = g.get_repo(full_name_or_id='https://github.com/Phantom-Thief/IA_Music')
+    tags = repository.get_tags()
+    matched_tags = [match for match in tags if match.name == tag]
+    if not matched_tags:
+        raise ValueError('No Tag or Branch exists with that name')
+    return matched_tags[0].commit.sha
+
+def download_directory(repository, sha, server_path):
+    """
+    Download all contents at server_path with commit tag sha in
+    the repository.
+    """
+    contents = repository.get_dir_contents(server_path, ref=sha)
+
+    for content in contents:
+        print ("Processing %s" % content.path)
+        if content.type == 'dir':
+            download_directory(repository, sha, content.path)
+        else:
+            path = content.path
+            file_content = repository.get_contents(path, ref=sha)
+            file_data = base64.b64decode(file_content.content)
+            file_out = open(content.name, "wb")
+            file_out.write(file_data)
+            file_out.close()
+
+
+def download_directory_from_git(tag,directory):
+    g = Github()
+
+    repository = g.get_repo(full_name_or_id='Phantom-Thief/IA_Music')
+    
+    branch_or_tag_to_download = str(tag)
+    sha = get_sha_for_tag(repository, branch_or_tag_to_download)
+
+    directory_to_download = str(directory)
+    download_directory(repository, sha, directory_to_download)
--- updater.py
+++ updater.py
@@ -7,6 +7,57 @@ import shutil
 
 urllib3.disable_warnings()
 
+# https://sookocheff.com/post/tools/downloading-directories-of-code-from-github-using-the-github-api/
+
+from github import Github
+import base64
+
+def get_sha_for_tag(repository, tag):
+    """
+    Returns a commit PyGithub object for the specified repository and tag.
+    """
+    branches = repository.get_branches()
+    matched_branches = [match for match in branches if match.name == tag]
+    if matched_branches:
+        return matched_branches[0].commit.sha
+
+    tags = repository.get_tags()
+    matched_tags = [match for match in tags if match.name == tag]
+    if not matched_tags:
+        raise ValueError('No Tag or Branch exists with that name')
+    return matched_tags[0].commit.sha
+
+def download_directory(repository, sha, server_path):
+    """
+    Download all contents at server_path with commit tag sha in
+    the repository.
+    """
+    contents = repository.get_dir_contents(server_path, ref=sha)
+
+    for content in contents:
+        print ("Processing %s" % content.path)
+        if content.type == 'dir':
+            download_directory(repository, sha, content.path)
+        else:
+            path = content.path
+            file_content = repository.get_contents(path, ref=sha)
+            file_data = base64.b64decode(file_content.content)
+            file_out = open(content.name, "wb")
+            file_out.write(file_data)
+            file_out.close()
+
+
+def download_directory_from_git(tag,directory):
+    g = Github()
+
+    repository = g.get_repo(full_name_or_id='Phantom-Thief/IA_Music')
+    
+    branch_or_tag_to_download = str(tag)
+    sha = get_sha_for_tag(repository, branch_or_tag_to_download)
+
+    directory_to_download = str(directory)
+    download_directory(repository, sha, directory_to_download)
+
 def updater():
     release_tag = requests.get(
         'http://api.github.com/repos/Phantom-Thief/IA_Music/releases/latest', verify = False
@@ -20,22 +71,9 @@ def updater():
 
     print('Launching update')
 
-    zipurl = requests.get('http://api.github.com/repos/Phantom-Thief/IA_Music/releases/latest', verify = False).json()['zipball_url']
+    path = os.getcwd()
+    print ("The current working directory is %s" % path)
+    os.mkdir(release_tag)
 
-    resp = requests.get(zipurl)
-
-    print('Update downloaded')
-
-    zname = release_tag
-    zfile = open(zname, 'wb')
-    zfile.write(resp.content)
-    zfile.close()
-
-    with zipfile.ZipFile(release_tag, 'r') as zip_ref:
-        zip_ref.extractall('tmp')
-
-    shutil.rmtree('/'+current_tag, ignore_errors=True)
-    with open('version.txt','w') as f:
-        f.write(release_tag)
-
-    print('Update installed')
+    
+updater()
