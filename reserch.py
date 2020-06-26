cf24b6067cd0ee8c89ccc2cfa26f0a5828ac8937
Menchit-ai <thomas.menchi@edu.esiee.fr>
2020-06-26 09:59:07 +0200

    méngae

--- reserch.py
+++ reserch.py
@@ -1,12 +1,35 @@
 import requests
 import urllib3
 import json
+import time
 
+import requests, zipfile, io
+
+
+
+start_time = time.time()
 urllib3.disable_warnings()
 # GET /repos/:owner/:repo/releases
 
+release_tag = requests.get('http://api.github.com/repos/Phantom-Thief/IA_Music/releases/latest', verify = False).json()['tag_name'].strip()
+
+current_tag = None
+with open('version.txt','r') as f:
+    current_tag = f.read().strip()
+
+print(release_tag)
+print(current_tag)
+print(release_tag==current_tag)
+
+
+print("Durée d'exécution : {}s".format(time.time()-start_time))
+
+
+
 query = requests.get('http://api.github.com/repos/Phantom-Thief/IA_Music/releases/latest', verify = False).json()
 
-print(
-    query['tag_name']
-)
+
+
+print([i for i in query])
+
+requests.get(query['tarball_url'])
